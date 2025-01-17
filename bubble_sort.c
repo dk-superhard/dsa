c32c2401da6a51bc4020170480424768bf76448c
Dineshkumar <dk.superhard@outlook.com>
2025-01-17 20:39:56 +0530

    added SUCCESS macro definition

--- bubble_sort.c
+++ bubble_sort.c
@@ -2,13 +2,15 @@
 
 #include <stdio.h>
 
+#define SUCCESS 0
+
 void print_array(int arr[], int arr_size);
 void bubble_sort(int arr[], int arr_size);
 void swap(int *elem1, int *elem2);
 
 int main()
 {
-    int arr[] = {15, 12, 8, 7, 6};
+    int arr[] = {15, 12, 8, 7, 6, 0, -7, -18, 100};
     int arr_size = sizeof(arr)/sizeof(arr[0]);
     print_array(arr, arr_size);
     bubble_sort(arr, arr_size);
@@ -26,8 +28,8 @@ void print_array(int arr[], int arr_size)
 
 void bubble_sort(int arr[], int arr_size)
 {
-    for( int i=0; i<size-1; i++) {
-        for ( int j=0; j<size-i-1; j++) {
+    for( int i=0; i<arr_size-1; i++) {
+        for ( int j=0; j<arr_size-i-1; j++) {
             if (arr[j] > arr[j+1]) {
                 swap(&arr[j], &arr[j+1]);
             }
