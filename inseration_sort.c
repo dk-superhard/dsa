992ed8fb7a8f5a3f34730df4d01a3e38f0c8ecd0
Dineshkumar <dk.superhard@outlook.com>
2025-01-20 10:10:46 +0530

    Insertion Sort is added

---
+++ inseration_sort.c
@@ -0,0 +1,47 @@
+/* Implements insertion sort to sort all elements in an array */
+
+#include <stdio.h>
+
+#define SUCCESS 0
+
+void print_array(int arr[], int arr_size);
+void insertion_sort(int arr[], int arr_size);
+
+int main()
+{
+    int arr[] = {-18,-7,0,6,7,8,12,15,100};
+    int arr_size = sizeof(arr)/sizeof(arr[0]);
+    print_array(arr, arr_size);
+    insertion_sort(arr, arr_size);
+    print_array(arr, arr_size);
+    return SUCCESS;
+}
+
+void print_array(int arr[], int arr_size)
+{
+    for ( int i=0; i<arr_size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void insertion_sort(int arr[], int arr_size)
+{
+    int temp;
+    int j;
+    for( int i=1; i<arr_size; i++) {
+        if (arr[i] > arr[i-1]) {
+            continue;
+        }
+        temp = arr[i];
+        for( j=i-1; j >= 0; j--) {
+            if (temp < arr[j]) {
+                arr[j+1] = arr[j];
+            }
+        }
+        printf("%d\n", temp);
+        printf("j=%d\n", j);
+        arr[++j] = temp;
+        print_array(arr, arr_size);
+    }
+}
